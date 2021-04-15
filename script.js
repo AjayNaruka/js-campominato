e8d1c3d2051444614233e5b63210a65e4c003073
AjayNaruka <naruka.ajay1997@gmail.com>
2021-04-15 15:24:30 +0200

    FINE

--- script.js
+++ script.js
@@ -1,14 +1,43 @@
-var limit = 10;
+var difficulty = prompt("Inserisci la difficolta' (0, 1, 2) ");
+console.log("Hai scelto la difficolta' : "+ difficulty);
+
+var bombArr = [];
+var nBombs = 16;
+var limit;
+
+switch(difficulty){
+  case "0":
+    var limit = 100;
+    break;
+
+  case "1":
+    var limit = 80;
+    break;
+  
+    case "2":
+      var limit = 50;
+
+      break;
+}
+
+console.log("Limit a: "+limit);
+
+bombGen(nBombs,limit,bombArr);
+
+console.log("Iniziamo il gioco: \n");
+playGame(nBombs,limit,bombArr);
+
+
+/* var limit = 10;
 var bombArr = [];
 var nBombs = 5;
+
 
 
 bombGen(nBombs,limit,bombArr);
 
 console.log("Iniziamo il gioco: \n");
-playGame(nBombs,limit,bombArr)
-
-
+playGame(nBombs,limit,bombArr); */
 //FUNZIONI//
 
 
@@ -37,18 +66,26 @@ function playGame (n,l,array){
   var winning = true;
   var round =0;
   var forcedStop = false;
-  while(winning && round<=(l-n)){
-    console.log("Mi trovo al round numero : \n " + round)
-    var userIndex = prompt("Inserisci un indice in cui testare ");
-    console.log("Hai inserito l'indice :\n" + userIndex);
+  while(winning && round<(l-n)){
+    console.log("Mi trovo al round numero : \n" +"*******\n"+ round +"\n*******")
+    var userIndex = prompt("Inserisci un indice in cui testare (STOP per interrompere)");
+    console.log("Hai inserito l'indice :\n " + userIndex);
     //PREMATURE ENDING
     if(userIndex == 'STOP'){
       winning = false;
       forcedStop = true;
       break;
     }
-
-    //CHECK RANGE
+    var valid = isValid(userIndex,0,l,userInputs);
+    if(valid){
+      userInputs.push(userIndex);
+      console.log("Numero inserito VALIDO");
+      round++;
+      if(array[userIndex]=='X'){
+        winning =false;
+      } 
+    }
+   /*  //CHECK RANGE
     if(userIndex>l){
       console.log("Hai inserito un numero NON valido. Ritenta")
       round--;
@@ -58,18 +95,43 @@ function playGame (n,l,array){
     else {
       console.log("Indice già inserito in precedenza");
      // round--;
-    }
+    }*/
     
-    if(array[userIndex]=='X'){
-      winning =false;
-    }
+   
     
-    round++;
   }
   if(forcedStop) console.log("Gioco interrotto")
-  if(winning && !forcedStop) console.log ("HAI VINTO!")
-  else if(!winning && !forcedStop) console.log("Hai colpito una bomba");
+  if(winning && !forcedStop) console.log ("-- HAI VINTO! --")
+  else if(!winning && !forcedStop) console.log("-- Hai colpito una bomba --");
 
 }
 
+function isValid(index, min, max, pastChoices){
+  if(index<min || index > max) {
+  console.log("Numero fuori range")
+  return false;
+  }
+  if(isNaN(index)){
+    console.log("Non hai inserito un numero")
+    return false;
+  }
+  if(pastChoices.includes(index)){
+    console.log("Numero già inserito")
+    return false
+  }
+  return true
+}
 
+
+//// TESTS DOUBLE FUNCTION
+function sum(){
+  var a = genNum();
+  var b = genNum();
+  console.log("a = "+a)
+  console.log("b = "+b)
+  console.log(a+b);
+}
+
+function genNum(){
+  return Math.ceil(Math.random()*5);
+}
